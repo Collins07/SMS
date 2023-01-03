1d75e1addbe1f8800d51334422bc8d027c9d3936
Collins07 <collinsabaya07@gmail.com>
2023-01-03 16:00:49 +0300

    template inheritance

--- student/views.py
+++ student/views.py
@@ -24,3 +24,24 @@ def add(request):
             new_email = form.cleaned_data['email']
             new_field_of_study= form.cleaned_data['field_of_study']
             new_gpa = form.cleaned_data['gpa']
+
+            new_student = Student (
+                student_number = new_student_number,
+                first_name = new_first_name,
+                last_name = new_last_name,
+                email = new_email,
+                field_of_study = new_field_of_study,
+                gpa = new_gpa,
+            )
+
+            new_student.save()
+            return render(request, 'students/add.html',{
+                'form': StudentForm(),
+                'success': True
+            })
+
+    else:
+        form = StudentForm()
+        return render(request, 'students/add.html', {
+            'form': StudentForm()
+        })
