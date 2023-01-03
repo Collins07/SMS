edfbf63cf7f664680b73b5eb58209655ecd1f91d
Collins07 <collinsabaya07@gmail.com>
2023-01-03 17:19:54 +0300

    CRUD full implementation

--- student/views.py
+++ student/views.py
@@ -45,3 +45,20 @@ def add(request):
         return render(request, 'students/add.html', {
             'form': StudentForm()
         })
+
+def edit(request, id):
+    if request.method == 'POST':
+        student = Student.objects.get(pk=id)
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return render(request, 'students/edit.html',{
+                'form': form,
+                'success': True
+                })
+    else:
+        student = Student.objects.get(pk=id)
+        form = StudentForm(request.POST, instance=student)
+    return render(request, 'students/edit.html',{
+        'form' : form
+    })
